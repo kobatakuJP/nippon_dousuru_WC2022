bac61b62394ddae0eeae9ec750222f3374d48d94
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2022-12-01 15:45:55 +0900

    とりまプッシュ

--- docs/js/js.js
+++ docs/js/js.js
@@ -1,6 +1,15 @@
+function koushin(data) {
+    const scores = data.current_score
+    const japan = scores[0].score[0]
+    const spain = scores[0].score[1]
+    const costarica = scores[1].score[0]
+    const german = scores[1].score[1]
+    alert("japan", japan, "spain", spain, "costarica", costarica, "german", german)
+}
+
 function getData() {
     fetch('./data.json')
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => koushin(data));
 }
 getData()
